f6d1cead0cc6ed471a3fe620b2e48c55bb12cd77
Tamalika <tam29barsha@gmail.com>
2026-03-01 21:19:41 +0530

    this is commit from Tamalika

--- src/test/java/demo/PracticeProject/Practice.java
+++ src/test/java/demo/PracticeProject/Practice.java
@@ -19,35 +19,6 @@ public class Practice {
 		System.out.println("m1 method ");
 	}
 	
-	@Test(groups= {"smoke"})
-	public void m3( ) {
-		
-		System.out.println("m3 method ");
-	}
-	
-	@Test
-	public void m2() {
-		
-		System.out.println("m2 method ");
-	}
-	
-	@Test
-	public void m4() {
-		
-		System.out.println("m4 method ");
-	}
-
-//	@Test(priority=1)
-//	public void v1() {
-//		
-//		System.out.println("v1 method");
-//	}
-//	
-//	@Test(priority=1)
-//	public void v2() {
-//		
-//		System.out.println("v2 method");
-//	}
 	
 	
 }
